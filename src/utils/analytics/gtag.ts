e6a2eb5b338c37b057a3da652552db05db9b5355
watsuyo <dmwtpdmwtp24@gmail.com>
2021-09-18 20:57:21 +0900

    fix gtag types

--- src/utils/analytics/gtag.ts
+++ src/utils/analytics/gtag.ts
@@ -1,5 +1,6 @@
 export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || ''
 
+
 export const pageview = (url: string) => {
 	window.gtag('config', GA_TRACKING_ID, {
 		page_path: url,
