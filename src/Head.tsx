f49f26280d113ab7c6f4f869d53af19dc0433dc5
watsuyo <watsuyo.dev@gmail.com>
2023-12-07 21:03:38 +0100

    image を icon に変える

--- src/Head.tsx
+++ src/Head.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-import { SITE_NAME, ICON, PAGE_TYPE, DOMAIN } from 'global'
+import { SITE_NAME, ICON, PAGE_TYPE } from 'global'
 
 interface Props {
   title: string
@@ -12,23 +12,22 @@ interface Props {
 // eslint-disable-next-line react/display-name
 export default ({ title, description, keyword, url }: Props): JSX.Element => {
   const TITLE = SITE_NAME === title ? SITE_NAME : `${SITE_NAME} | ${title}`
-  const IMAGE = `${DOMAIN}/api/og?title=${TITLE}`
   return (
     <Head>
       <title>{TITLE}</title>
       <meta name="keywords" content={keyword} />
       <meta name="description" content={description} />
-      <meta property="og:image" content={IMAGE} />
+      <meta property="og:image" content={ICON} />
       <meta property="og:type" content={PAGE_TYPE} />
       <meta property="og:title" content={TITLE} />
       <meta property="og:description" content={description} />
       <meta property="og:site_name" content={SITE_NAME} />
-      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="@watsuyo_2" />
       <meta name="twitter:url" content={url} />
       <meta name="twitter:title" content={TITLE} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={IMAGE} />
+      <meta name="twitter:image" content={ICON} />
       <link rel="icon" href={ICON} />
       <link rel="canonical" href={url} />
       <link rel="shortcut icon" href={ICON} />
